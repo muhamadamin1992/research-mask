59a5132f83d33a76e2bc8adcdbed4281321b9f0d
Muhamadamin <muhamadamin1992@gmail.com>
2020-07-20 14:13:00 +0500

    deploy to github pages

---
+++ dist/precache-manifest.8f044e9edb2f1ed97780456a1e7e946a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba28b06b8dfda8887188",
+    "url": "/css/app.4773283c.css"
+  },
+  {
+    "revision": "0f499124548fc2ff2287",
+    "url": "/css/chunk-vendors.52abf2c7.css"
+  },
+  {
+    "revision": "fdf634c7d8cee9293d8db750cb9e2123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd46a498b371a17de72a",
+    "url": "/js/about.8e6809ae.js"
+  },
+  {
+    "revision": "ba28b06b8dfda8887188",
+    "url": "/js/app.441402f6.js"
+  },
+  {
+    "revision": "0f499124548fc2ff2287",
+    "url": "/js/chunk-vendors.749fccef.js"
+  },
+  {
+    "revision": "00ed4e966a1cb11a04b2",
+    "url": "/js/cleave.221541aa.js"
+  },
+  {
+    "revision": "4a21fc4260515e907c36",
+    "url": "/js/money.51e19a3c.js"
+  },
+  {
+    "revision": "954898734c010ddc5a5e",
+    "url": "/js/v-mask.33b30511.js"
+  },
+  {
+    "revision": "fe1e758580841995c700",
+    "url": "/js/vanilla-masker.3d3c7a66.js"
+  },
+  {
+    "revision": "82ba326341c02c804867",
+    "url": "/js/vue-currency-input.a6a78486.js"
+  },
+  {
+    "revision": "69bbc623428ff2669f59e7eb490aebfa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
